7cb8ede8f61261329fc9320858bf181d0437262d
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-22 13:35:19 +0200

    feat: Menu component

---
+++ src/components/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import style from './Header.scss';
+
+const Menu = () => (
+    <div className={style.menuContainer}>
+        Menu
+    </div>
+);
+
+export default Menu;

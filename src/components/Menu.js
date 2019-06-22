216c74e72fea7b4f6857d21d3486f4cdae2b6208
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-22 16:48:41 +0200

    feat: collapsable submenus

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import style from './Menu.scss';
 
-const Menu = () => (
-    <div className={style.menuContainer}>
-        <div className={style.menuCategory}>Books</div>
-        <div className={style.menuCategory}>Genres</div>
-    </div>
-);
+const Menu = () => {
+    const [isShowSubMenu, setIsShowSubMenu] = useState({
+        books: false,
+        genres: false
+    });
+
+    const toggleShowSubMenu = event => {
+        const { target } = event;
+        const { id } = target;
+        setIsShowSubMenu(
+            id === 'books'
+                ? { books: !isShowSubMenu[id], genres: false }
+                : { genres: !isShowSubMenu[id], books: false }
+        );
+    };
+
+    return (
+        <div className={style.menuContainer}>
+            <button
+                id="books"
+                className={style.menuCategory}
+                onClick={toggleShowSubMenu}
+                type="button"
+            >
+                {isShowSubMenu.books && (
+                    <div className={style.subMenu}>
+                        <ul>
+                            <li>View</li>
+                            <li>Create</li>
+                            <li>Delete</li>
+                            <li>Update</li>
+                        </ul>
+                    </div>
+                )}
+                Books
+            </button>
+            <button
+                id="genres"
+                className={style.menuCategory}
+                onClick={toggleShowSubMenu}
+                type="button"
+            >
+                {isShowSubMenu.genres && (
+                    <div className={style.subMenu}>
+                        <ul>
+                            <li>View</li>
+                            <li>Create</li>
+                            <li>Delete</li>
+                            <li>Update</li>
+                        </ul>
+                    </div>
+                )}
+                Genres
+            </button>
+        </div>
+    );
+};
 
 export default Menu;

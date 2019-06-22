c47525f8045837f01f7c09eeee718513649496c2
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-22 13:34:57 +0200

    feat: Logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import style from './Logo.scss';
+
+const Logo = () => (
+    <div className={style.logoContainer}>
+        Logo
+    </div>
+);
+
+export default Logo;

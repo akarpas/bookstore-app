36e010b73c357c69581f93dfb8ccab65af2892ca
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-24 16:20:18 +0200

    feat: implement View component for books

--- src/components/View.js
+++ src/components/View.js
@@ -1,17 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { fetchItems } from '../actions/items';
 import style from './View.scss';
 
 const View = props => {
-    const { match } = props;
+    const [items, setItems] = useState([]);
+    const { match, books, booksLoading } = props;
     const { params } = match;
     const { category } = params;
+    const isBooks = category === 'books';
 
-    return <div className={style.headerContainer}>View {category}!</div>;
+    useEffect(() => {
+        props.fetchItems(category);
+        setItems(isBooks ? books : []);
+    }, []);
+
+    useEffect(() => {
+        props.fetchItems(category);
+    }, [category]);
+
+    const itemsLoading = isBooks ? booksLoading : [];
+
+    return (
+        <div className={style.viewContainer}>
+            <h2 className={style.viewHeader}>List of {category}</h2>
+            {itemsLoading ? (
+                <div>Loading...</div>
+            ) : (
+                <table cellSpacing="0">
+                    <tbody>
+                        <tr>
+                            <th>Title: </th>
+                            <th>Genre: </th>
+                            <th>Price: </th>
+                        </tr>
+                        {items.map(book => (
+                            <tr key={`${book.title}row`}>
+                                <td key={book.title}>{book.title}</td>
+                                <td key={book.title + book.genre}>
+                                    {book.genre}
+                                </td>
+                                <td
+                                    className={style.bookPrice}
+                                    key={book.title + book.price}
+                                >
+                                    {book.price} {book.currency}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
+    );
 };
 
-export default View;
+const mapStateToProps = state => {
+    return {
+        books: state.books.booksList,
+        booksLoading: state.books.booksLoading
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchItems: category => dispatch(fetchItems(category))
+    };
+};
+
+export default withRouter(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )(View)
+);
 
 View.propTypes = {
+    booksLoading: PropTypes.bool,
+    books: PropTypes.array,
+    fetchItems: PropTypes.func,
     match: PropTypes.object
 };

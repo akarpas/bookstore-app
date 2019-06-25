8e65b628f009721bb37950453812e2b51b3bbb30
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-25 10:23:09 +0200

    fix: internet explorer compatibility with polyfills and fixing redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
@@ -6,15 +7,14 @@ import rootReducer from './reducers';
 
 const middleware = applyMiddleware(thunk, logger);
 
-const reduxDevTools =
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__();
+// const reduxDevTools =
+//     window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//     window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const store = createStore(
     rootReducer,
-    compose(
-        middleware,
-        reduxDevTools
+    composeWithDevTools(
+        middleware
     )
 );
 

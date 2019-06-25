a42bfdb1fbdb58df8ac9f46d3ca3d5360e273128
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-25 17:03:04 +0200

    chore: add conditional class to button for updates and fix change of route issue

--- src/components/Update.js
+++ src/components/Update.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import cloneDeep from 'lodash.clonedeep';
 import { fetchItems, updateItem } from '../actions/items';
-import style from './Delete.scss';
+import style from './Update.scss';
 
 const Update = props => {
     const { match, booksLoading, books, genres, genresLoading } = props;
@@ -13,15 +13,24 @@ const Update = props => {
     const { params } = match;
     const { category } = params;
     const isBooks = category === 'books';
-    const [itemValues, setItemValues] = useState({ books: cloneBooks, genres: cloneGenres } || []);
+    const [itemValues, setItemValues] = useState(
+        { books: cloneBooks, genres: cloneGenres } || []
+    );
 
     useEffect(() => {
         props.fetchItems(category);
     }, []);
 
+    useEffect(() => {
+        props.fetchItems(category);
+        setItemValues({ books: cloneBooks, genres: cloneGenres });
+    }, [category]);
+
     const handleInputChange = event => {
         const [field, id] = event.target.id.split('-');
-        const itemToUpdate = itemValues[category].findIndex(item => item.id === id);
+        const itemToUpdate = itemValues[category].findIndex(
+            item => item.id === id
+        );
         const newValues = itemValues[category].splice(0);
         newValues[itemToUpdate][field] = event.target.value;
         setItemValues({ [category]: newValues });
@@ -47,62 +56,82 @@ const Update = props => {
                             <th>Price: </th>
                             <th>Currency: </th>
                         </tr>
-                        {content.map((book, index) => (
-                            <tr key={`${index}row`}>
-                                <td>
-                                    <button
-                                        onClick={handleUpdateItem}
-                                        type="button"
-                                        id={book.id}
+                        {content.map((book, index) => {
+                            const hasUpdates =
+                                content[index].title !== books[index].title ||
+                                content[index].price !== books[index].price ||
+                                content[index].genre !== books[index].genre ||
+                                content[index].currency !==
+                                    books[index].currency;
+
+                            return (
+                                <tr key={`${index}row`}>
+                                    <td>
+                                        <button
+                                            className={
+                                                hasUpdates
+                                                    ? style.buttonUpdate
+                                                    : style.button
+                                            }
+                                            onClick={handleUpdateItem}
+                                            type="button"
+                                            id={book.id}
+                                        >
+                                            Update
+                                        </button>
+                                    </td>
+                                    <td key={`${index}title`}>
+                                        <input
+                                            onChange={handleInputChange}
+                                            id={`title-${book.id}`}
+                                            type="text"
+                                            value={book.title}
+                                        />
+                                    </td>
+                                    <td key={`${index}genre`}>
+                                        <select
+                                            value={book.genre}
+                                            onChange={handleInputChange}
+                                            id={`genre-${book.id}`}
+                                        >
+                                            <option value="scienceFiction">
+                                                Science Fiction
+                                            </option>
+                                            <option value="mystery">
+                                                Mystery
+                                            </option>
+                                            <option value="romance">
+                                                Romance
+                                            </option>
+                                            <option value="horror">
+                                                Horror
+                                            </option>
+                                        </select>
+                                    </td>
+                                    <td key={`${index}price`}>
+                                        <input
+                                            onChange={handleInputChange}
+                                            id={`price-${book.id}`}
+                                            type="number"
+                                            value={book.price}
+                                        />
+                                    </td>
+                                    <td
+                                        className={style.bookPrice}
+                                        key={`${index}currency`}
                                     >
-                                        Update
-                                    </button>
-                                </td>
-                                <td key={`${index}title`}>
-                                    <input
-                                        onChange={handleInputChange}
-                                        id={`title-${book.id}`}
-                                        type="text"
-                                        value={book.title}
-                                    />
-                                </td>
-                                <td key={`${index}genre`}>
-                                    <select
-                                        value={book.genre}
-                                        onChange={handleInputChange}
-                                        id={`genre-${book.id}`}
-                                    >
-                                        <option value="scienceFiction">
-                                            Science Fiction
-                                        </option>
-                                        <option value="mystery">Mystery</option>
-                                        <option value="romance">Romance</option>
-                                        <option value="horror">Horror</option>
-                                    </select>
-                                </td>
-                                <td key={`${index}price`}>
-                                    <input
-                                        onChange={handleInputChange}
-                                        id={`price-${book.id}`}
-                                        type="number"
-                                        value={book.price}
-                                    />
-                                </td>
-                                <td
-                                    className={style.bookPrice}
-                                    key={`${index}currency`}
-                                >
-                                    <select
-                                        value={book.currency}
-                                        onChange={handleInputChange}
-                                        id={`currency-${book.id}`}
-                                    >
-                                        <option value="eur">EUR</option>
-                                        <option value="usd">USD</option>
-                                    </select>
-                                </td>
-                            </tr>
-                        ))}
+                                        <select
+                                            value={book.currency}
+                                            onChange={handleInputChange}
+                                            id={`currency-${book.id}`}
+                                        >
+                                            <option value="eur">EUR</option>
+                                            <option value="usd">USD</option>
+                                        </select>
+                                    </td>
+                                </tr>
+                            );
+                        })}
                     </tbody>
                 </table>
             );
@@ -115,27 +144,36 @@ const Update = props => {
                             <th>Update: </th>
                             <th>Genre: </th>
                         </tr>
-                        {content.map((genre, index) => (
-                            <tr key={`${index}row`}>
-                                <td>
-                                    <button
-                                        onClick={handleUpdateItem}
-                                        type="button"
-                                        id={genre.id}
-                                    >
-                                        Update
-                                    </button>
-                                </td>
-                                <td key={`${index}genre`}>
-                                    <input
-                                        onChange={handleInputChange}
-                                        id={`name-${genre.id}`}
-                                        type="text"
-                                        value={genre.name}
-                                    />
-                                </td>
-                            </tr>
-                        ))}
+                        {content.map((genre, index) => {
+                            const hasUpdates =
+                                content[index].name !== genres[index].name;
+                            return (
+                                <tr key={`${index}row`}>
+                                    <td>
+                                        <button
+                                            className={
+                                                hasUpdates
+                                                    ? style.buttonUpdate
+                                                    : style.button
+                                            }
+                                            onClick={handleUpdateItem}
+                                            type="button"
+                                            id={genre.id}
+                                        >
+                                            Update
+                                        </button>
+                                    </td>
+                                    <td key={`${index}genre`}>
+                                        <input
+                                            onChange={handleInputChange}
+                                            id={`name-${genre.id}`}
+                                            type="text"
+                                            value={genre.name}
+                                        />
+                                    </td>
+                                </tr>
+                            );
+                        })}
                     </tbody>
                 </table>
             );
@@ -145,12 +183,12 @@ const Update = props => {
     const itemsLoading = isBooks ? booksLoading : genresLoading;
 
     return (
-        <div className={style.deleteContainer}>
-            <h2 className={style.deleteHeader}>Update {category}</h2>
+        <div className={style.updateContainer}>
+            <h2 className={style.updateHeader}>Update {category}</h2>
             {itemsLoading ? (
                 <div>Loading...</div>
             ) : (
-                render[category](itemValues[category])
+                itemValues[category] && render[category](itemValues[category])
             )}
         </div>
     );

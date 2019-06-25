62ac3a7484c9a737f586cbf39c67bc1986cfa3e8
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-25 11:51:20 +0200

    chore: add category to payload

--- src/actions/items.js
+++ src/actions/items.js
@@ -4,22 +4,27 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 const actions = {
     loading: (category, isLoading) => ({
+        category,
         type: `${category}Loading`,
         payload: isLoading
     }),
     set: (category) => ({
+        category,
         type: `set${toPascalCase(category)}`,
         payload: null
     }),
     create: (category, item) => ({
+        category,
         type: `create${toPascalCase(category.slice(0, category.length - 1))}`,
         payload: item
     }),
     delete: (category, id) => ({
+        category,
         type: `delete${toPascalCase(category.slice(0, category.length - 1))}`,
         payload: id
     }),
     update: (category, item) => ({
+        category,
         type: `update${toPascalCase(category.slice(0, category.length - 1))}`,
         payload: item
     })

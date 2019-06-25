ae62d8879b1fc6f8c523824c5319c37cfc7f6d04
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-25 11:51:35 +0200

    refactor: simplify reducer to reuse functions

--- src/reducers/items.js
+++ src/reducers/items.js
@@ -20,92 +20,62 @@ const INITIAL_STATE = {
     genresLoading: false
 };
 
-const setBooks = state => {
-    return { ...state, booksLoading: false };
+const setItems = (state, category) => {
+    return { ...state, [`${category}Loading`]: false };
 };
 
-const setBooksLoading = state => {
-    return { ...state, booksLoading: true };
+const setLoading = (state, category) => {
+    return { ...state, [`${category}Loading`]: true };
 };
 
-const setGenres = state => {
-    return { ...state, genresLoading: false };
-};
-
-const setGenresLoading = state => {
-    return { ...state, genresLoading: true };
-};
-
-const deleteBook = (state, payload) => {
-    const currentBooks = state.booksList.slice(0);
-    const newBooks = currentBooks.filter(book =>
-        Number(book.id) !== Number(payload)
+const deleteItem = (state, payload, category) => {
+    const list = `${category}List`;
+    const currentItems = state[list].slice(0);
+    const newItems = currentItems.filter(item =>
+        Number(item.id) !== Number(payload)
     );
-    return { ...state, booksList: newBooks };
+    return { ...state, [list]: newItems };
 };
 
-const deleteGenre = (state, payload) => {
-    const currentGenres = state.genresList.slice(0);
-    const newGenres = currentGenres.filter(genre =>
-        Number(genre.id) !== Number(payload)
+const updateItem = (state, payload, category) => {
+    const list = `${category}List`;
+    const { id } = payload;
+    const currentItems = state[list].slice(0);
+    const index = currentItems.findIndex((item => Number(item.id) === Number(id)));
+    currentItems[index] = payload;
+    return { ...state, [list]: currentItems };
+};
+
+const createItem = (state, payload, category) => {
+    const list = `${category}List`;
+    const isBooks = category === 'books';
+    const currentItems = state[list].slice(0);
+    const itemIds = currentItems.map(item => item.id);
+    const lastId = Math.max.apply(null, itemIds);
+    currentItems.push(isBooks
+        ? { ...payload, id: String(lastId + 1) }
+        : { ...payload, name: payload.genreName, id: String(lastId + 1) }
     );
-    return { ...state, genresList: newGenres };
-};
-
-const updateBook = (state, payload) => {
-    const { id } = payload;
-    const currentBooks = state.booksList.slice(0);
-    const index = currentBooks.findIndex((book => Number(book.id) === Number(id)));
-    currentBooks[index] = payload;
-    return { ...state, booksList: currentBooks };
-};
-
-const updateGenre = (state, payload) => {
-    const { id } = payload;
-    const currentGenres = state.genresList.slice(0);
-    const index = currentGenres.findIndex((genre => Number(genre.id) === Number(id)));
-    currentGenres[index] = payload;
-    return { ...state, genresList: currentGenres };
-};
-
-const createBook = (state, payload) => {
-    const currentBooks = state.booksList.slice(0);
-    const bookIds = currentBooks.map(book => book.id);
-    const lastId = Math.max.apply(null, bookIds);
-    currentBooks.push({ ...payload, id: String(lastId + 1) });
-    return { ...state, booksList: currentBooks };
-};
-
-const createGenre = (state, payload) => {
-    const currentGenres = state.genresList.slice(0);
-    const genreIds = currentGenres.map(genre => genre.id);
-    const lastId = Math.max.apply(null, genreIds);
-    currentGenres.push({ ...payload, name: payload.genreName, id: String(lastId + 1) });
-    return { ...state, genresList: currentGenres };
+    return { ...state, [list]: currentItems };
 };
 
 export function items(state = INITIAL_STATE, action) { // eslint-disable-line
     switch (action.type) {
     case SET_BOOKS:
-        return setBooks(state);
-    case BOOKS_LOADING:
-        return setBooksLoading(state);
-    case DELETE_BOOK:
-        return deleteBook(state, action.payload);
-    case UPDATE_BOOK:
-        return updateBook(state, action.payload);
-    case CREATE_BOOK:
-        return createBook(state, action.payload);
     case SET_GENRES:
-        return setGenres(state);
+        return setItems(state, action.category);
+    case BOOKS_LOADING:
     case GENRES_LOADING:
-        return setGenresLoading(state);
-    case CREATE_GENRE:
-        return createGenre(state, action.payload);
+        return setLoading(state, action.category);
+    case DELETE_BOOK:
     case DELETE_GENRE:
-        return deleteGenre(state, action.payload);
+        return deleteItem(state, action.payload, action.category);
+    case UPDATE_BOOK:
     case UPDATE_GENRE:
-        return updateGenre(state, action.payload);
+        return updateItem(state, action.payload, action.category);
+    case CREATE_BOOK:
+    case CREATE_GENRE:
+        return createItem(state, action.payload, action.category);
     default:
         return state;
     }

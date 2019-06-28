e28298fc377176565fbddd80dd0f29ce6081c54f
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-28 10:14:42 +0200

    feat: basic search input to filter books by search term

--- src/components/ViewList.js
+++ src/components/ViewList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import cloneDeep from 'lodash.clonedeep';
 import { compare } from '../utils/compare';
@@ -6,34 +6,52 @@ import style from './ViewList.scss';
 
 const ViewList = props => {
     const { category, content } = props;
+    const [searchInput, setSearchInput] = useState('');
+    const handleChange = event => {
+        setSearchInput(event.target.value);
+    };
 
     const render = {
         books: items => {
             const itemsCopy = cloneDeep(items);
             return (
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>Title: </th>
-                            <th>Genre: </th>
-                            <th>Price: </th>
-                        </tr>
-                        {itemsCopy.sort(compare).map(book => (
-                            <tr key={`${book.title}row`}>
-                                <td key={book.title}>{book.title}</td>
-                                <td key={book.title + book.genre}>
-                                    {book.genre}
-                                </td>
-                                <td
-                                    className={style.bookPrice}
-                                    key={book.title + book.price}
-                                >
-                                    {book.price} {book.currency}
-                                </td>
+                <div style={{ width: '100%' }}>
+                    <input
+                        type="text"
+                        onChange={event => handleChange(event)}
+                    />
+                    <table>
+                        <tbody>
+                            <tr>
+                                <th>Title: </th>
+                                <th>Genre: </th>
+                                <th>Price: </th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                            {itemsCopy
+                                .sort(compare)
+                                .filter(book => {
+                                    if (searchInput.length === 0) return book;
+                                    return book.title
+                                        .toLowerCase()
+                                        .includes(searchInput.toLowerCase());
+                                })
+                                .map(book => (
+                                    <tr key={`${book.title}row`}>
+                                        <td key={book.title}>{book.title}</td>
+                                        <td key={book.title + book.genre}>
+                                            {book.genre}
+                                        </td>
+                                        <td
+                                            className={style.bookPrice}
+                                            key={book.title + book.price}
+                                        >
+                                            {book.price} {book.currency}
+                                        </td>
+                                    </tr>
+                                ))}
+                        </tbody>
+                    </table>
+                </div>
             );
         },
         genres: items => {

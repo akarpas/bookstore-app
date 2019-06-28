a01affe7d9c25355caf4e2843a372b87d2c133f0
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-28 19:21:12 +0200

    feat: NoResults presentational component

---
+++ src/components/NoResults.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import style from './NoResults.scss';
+
+const NoResults = () => (
+    <div className={style.noResults}>
+        No Available Results
+    </div>
+);
+
+export default NoResults;

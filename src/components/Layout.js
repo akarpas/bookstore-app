125d30e1c40da04d45e6babc57c3bdb5151aad2f
Andreas Karpasitis <karpasitis@outlook.com>
2019-06-28 09:22:41 +0200

    feat: layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import style from './Layout.scss';
+
+const Layout = props => {
+    const { children, title } = props;
+    return (
+        <div className={style.layoutContainer}>
+            <h2 className={style.layoutHeader}>{title}</h2>
+            {children}
+        </div>
+    );
+};
+
+export default Layout;
+
+Layout.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.array,
+        PropTypes.object
+    ]),
+    title: PropTypes.string
+};
